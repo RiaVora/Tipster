d92e967fa79ecfacca921f0095d796c201194397
Ria Vora <riavora@fb.com>
2020-06-23 13:29:01 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by Ria Vora on 6/23/20.
+//  Copyright © 2020 Ria Vora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
